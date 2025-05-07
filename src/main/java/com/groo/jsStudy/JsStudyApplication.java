727573edbc739d125b2c57a2857088b82be2045e
박재권 <kogr0131@gmail.com>
2025-05-07 19:25:30 +0900

    js

---
+++ src/main/java/com/groo/jsStudy/JsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.groo.jsStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groo/jsStudy/JsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groo.jsStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
